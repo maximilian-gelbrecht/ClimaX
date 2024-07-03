13572fbcce7ac69ee15200f9d09630c8a9e96d82
Max <maximilian.gelbrecht@posteo.de>
2024-07-03 18:16:13 +0200

    enso index update

--- src/data_preprocessing/nc2np_enso_daily.py
+++ src/data_preprocessing/nc2np_enso_daily.py
@@ -6,10 +6,18 @@ import numpy as np
 import xarray as xr
 from tqdm import tqdm
 
-DAYS_PER_YEAR = 365
+def nc2np_enso_daily(path: str, save_dir: str, years, N_days_rolling=30, lons=[190, 240], lats=[-5, 5], normalize: bool=False):
+    """Computes a daily rolling mean ENSO index directly from netCDF Files and saves it as .npz files.
 
-def nc2np_enso_daily(path, save_dir, years, N_days_rolling=30, lons=[-170, -120], lats=[-5, 5], normalize: bool=False):
-
+    Args:
+        path (str): path to the netCDF files 
+        save_dir (str): path to location the ENSO index will be saved
+        years (range): years to compute the index for
+        N_days_rolling (int, optional): Length of rolling mean. Defaults to 30.
+        lons (list, optional): Longitudes of ENSO box. Defaults to [190, 240].
+        lats (list, optional): Latitudes of ENSO box. Defaults to [-5, 5].
+        normalize (bool, optional): Normalize the anomalies?. Defaults to False.
+    """
     assert lons[1] > lons[0] 
     assert lats[1] > lats[0]
 
@@ -25,13 +33,13 @@ def nc2np_enso_daily(path, save_dir, years, N_days_rolling=30, lons=[-170, -120]
 
         # get daily SST in ENSO box from hourly ERA5 SST data
         ps = glob.glob(os.path.join(path, f"*{year}*.nc"))
-        ds_sst = xr.open_mfdataset(path, combine="by_coopwrds", parallel=True)
+        ds_sst = xr.open_mfdataset(ps, combine="by_coords", parallel=True)
         
-        cropped_ds_sst = ds_sst.sel(lat=slice(min_lat,max_lat), lon=slice(min_lon,max_lon))
-        daily_sst_box[year] = cropped_ds_sst.resample(time='1D').mean('time')[:DAYS_PER_YEAR]
+        cropped_ds_sst = ds_sst.sel(latitude=slice(max_lat,min_lat), longitude=slice(min_lon,max_lon))
+        daily_sst_box[year] = cropped_ds_sst.resample(time='1D').mean('time')
 
-    # merge daily_sst_box into one xarray ds
-    daily_sst_box = xr.concat(daily_sst_box.items())
+    # merge daily_sst_box into one xarray ds and load it into RAM
+    daily_sst_box = xr.concat(daily_sst_box.values(), 'time').load()
 
     # compute anomalies 
     gb = daily_sst_box.groupby('time.dayofyear')
@@ -44,36 +52,32 @@ def nc2np_enso_daily(path, save_dir, years, N_days_rolling=30, lons=[-170, -120]
         anom = anom / clim_std
     
     # aggregate to daily index 
-    enso_index_daily = anom.mean(axis=(1,2))
+    enso_index_daily = anom['sst'].mean(dim=('latitude', 'longitude'))
 
     # rolling mean
     enso_index_rolling = enso_index_daily.rolling(time=N_days_rolling).mean()
 
     # save 
-    np.savez(os.path.join(save_dir,"enso-rolling-index.npz"), enso_index_rolling.to_numpy())
-
-
-
+    np.savez(os.path.join(save_dir,"enso-rolling-index.npy"), enso_index_rolling.to_numpy())
+    np.savez(os.path.join(save_dir,"enso-rolling-time.npy"), enso_index_rolling['time'].to_numpy())
 
 @click.command()
-@click.option("--root_dir", type=click.Path(exists=True))
+@click.option("--root_dir", type=click.Path(exists=True), default='/p/projects/climate_data_central/reanalysis/ERA5/sst')
 @click.option("--save_dir", type=str)
 @click.option("--start_year", type=int, default=1979)
 @click.option("--end_year", type=int, default=2018)
-@click.option("--N_days_rolling", type=int, default=30)
+@click.option("--n_days_rolling", type=int, default=30)
 def main(
     root_dir,
     save_dir,
     start_year,
     end_year,
-    N_days_rolling,    
+    n_days_rolling    
 ):
     
     assert start_year < end_year 
-
     os.makedirs(save_dir, exist_ok=True)
-
-    nc2np_enso_daily(root_dir, save_dir, range(start_year, end_year), N_days_rolling)
+    nc2np_enso_daily(root_dir, save_dir, range(start_year, end_year), n_days_rolling)
 
 if __name__ == "__main__":
     main()
