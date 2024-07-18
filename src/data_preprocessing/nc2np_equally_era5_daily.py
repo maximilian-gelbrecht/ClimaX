4831c4bf2c1e4ae352ff2bfbfaccee364680003b
Alistair White <alistair@pik-potsdam.de>
2024-07-18 17:10:01 +0200

    Fix path and method call

--- src/data_preprocessing/nc2np_equally_era5_daily.py
+++ src/data_preprocessing/nc2np_equally_era5_daily.py
@@ -14,7 +14,10 @@ from climax.utils.data_utils import DEFAULT_PRESSURE_LEVELS, NAME_TO_VAR
 HOURS_PER_YEAR = 8760  # 365-day year
 DAYS_PER_YEAR = 365
 
-def nc2np_daily(path, variables, years, save_dir, partition, num_shards_per_year, aggregation_mode):
+
+def nc2np_daily(
+    path, variables, years, save_dir, partition, num_shards_per_year, aggregation_mode
+):
     os.makedirs(os.path.join(save_dir, partition), exist_ok=True)
 
     if partition == "train":
@@ -22,13 +25,15 @@ def nc2np_daily(path, variables, years, save_dir, partition, num_shards_per_year
         normalize_std = {}
     climatology = {}
 
-    constants = xr.open_mfdataset(os.path.join(path, "constants.nc"), combine="by_coords", parallel=True)
+    constants = xr.open_mfdataset(
+        os.path.join(path, "constants/constants_5.625deg.nc"), combine="by_coords", parallel=True
+    )
     constant_fields = ["land_sea_mask", "orography", "lattitude"]
     constant_values = {}
     for f in constant_fields:
-        constant_values[f] = np.expand_dims(constants[NAME_TO_VAR[f]].to_numpy(), axis=(0, 1)).repeat(
-            HOURS_PER_YEAR, axis=0
-        )
+        constant_values[f] = np.expand_dims(
+            constants[NAME_TO_VAR[f]].to_numpy(), axis=(0, 1)
+        ).repeat(HOURS_PER_YEAR, axis=0)
         if partition == "train":
             normalize_mean[f] = constant_values[f].mean(axis=(0, 2, 3))
             normalize_std[f] = constant_values[f].std(axis=(0, 2, 3))
@@ -43,20 +48,24 @@ def nc2np_daily(path, variables, years, save_dir, partition, num_shards_per_year
         # non-constant fields
         for var in variables:
             ps = glob.glob(os.path.join(path, var, f"*{year}*.nc"))
-            ds = xr.open_mfdataset(ps, combine="by_coords", parallel=True)  # dataset for a single variable
+            dataset = xr.open_mfdataset(
+                ps, combine="by_coords", parallel=True
+            )  # dataset for a single variable
             code = NAME_TO_VAR[var]
 
-            if len(ds[code].shape) == 3:  # surface level variables
-                ds[code] = ds[code].expand_dims("val", axis=1)
+            if len(dataset[code].shape) == 3:  # surface level variables
+                dataset[code] = dataset[code].expand_dims("val", axis=1)
 
                 # remove the last 24 hours if this year has 366 days
                 # aggreate to daily data, either by just taking one snapshot or by taking a mean
                 if aggregation_mode == "mean":
-                    np_vars[var] = ds[code].resample(time='1D').mean('time').to_numpy()[:DAYS_PER_YEAR]
+                    np_vars[var] = dataset[code].resample(time='1D').mean('time').to_numpy()[:DAYS_PER_YEAR]
                 elif aggregation_mode == "snapshot":
-                    np_vars[var] = ds[code][0:HOURS_PER_YEAR:24].to_numpy()
-                
-                if partition == "train":  # compute mean and std of each var in each year
+                    np_vars[var] = dataset[code][0:HOURS_PER_YEAR:24].to_numpy()
+
+                if (
+                    partition == "train"
+                ):  # compute mean and std of each var in each year
                     var_mean_yearly = np_vars[var].mean(axis=(0, 2, 3))
                     var_std_yearly = np_vars[var].std(axis=(0, 2, 3))
                     if var not in normalize_mean:
@@ -73,21 +82,25 @@ def nc2np_daily(path, variables, years, save_dir, partition, num_shards_per_year
                     climatology[var].append(clim_yearly)
 
             else:  # multiple-level variables, only use a subset
-                assert len(ds[code].shape) == 4
-                all_levels = ds["level"][:].to_numpy()
+                assert len(dataset[code].shape) == 4
+                all_levels = dataset["level"][:].to_numpy()
                 all_levels = np.intersect1d(all_levels, DEFAULT_PRESSURE_LEVELS)
                 for level in all_levels:
-                    ds_level = ds.sel(level=[level])
+                    ds_level = dataset.sel(level=[level])
                     level = int(level)
 
                     # remove the last 24 hours if this year has 366 days
                     # aggreate to daily data, either by just taking one snapshot or by taking a mean
                     if aggregation_mode == "mean":
-                        np_vars[f"{var}_{level}"] = ds_level[code].resample(time='1D').mean('time').to_numpy[:DAYS_PER_YEAR]
+                        np_vars[f"{var}_{level}"] = ds_level[code].resample(time='1D').mean('time').to_numpy()[:DAYS_PER_YEAR]
                     elif aggregation_mode == "snapshot":
-                        np_vars[f"{var}_{level}"] = ds_level[code][0:HOURS_PER_YEAR:24].to_numpy()
-                
-                    if partition == "train":  # compute mean and std of each var in each year
+                        np_vars[f"{var}_{level}"] = ds_level[code][
+                            0:HOURS_PER_YEAR:24
+                        ].to_numpy()
+
+                    if (
+                        partition == "train"
+                    ):  # compute mean and std of each var in each year
                         var_mean_yearly = np_vars[f"{var}_{level}"].mean(axis=(0, 2, 3))
                         var_std_yearly = np_vars[f"{var}_{level}"].std(axis=(0, 2, 3))
                         if var not in normalize_mean:
@@ -124,7 +137,11 @@ def nc2np_daily(path, variables, years, save_dir, partition, num_shards_per_year
             if var not in constant_fields:
                 mean, std = normalize_mean[var], normalize_std[var]
                 # var(X) = E[var(X|Y)] + var(E[X|Y])
-                variance = (std**2).mean(axis=0) + (mean**2).mean(axis=0) - mean.mean(axis=0) ** 2
+                variance = (
+                    (std**2).mean(axis=0)
+                    + (mean**2).mean(axis=0)
+                    - mean.mean(axis=0) ** 2
+                )
                 std = np.sqrt(variance)
                 # E[X] = E[E[X|Y]]
                 mean = mean.mean(axis=0)
@@ -141,6 +158,7 @@ def nc2np_daily(path, variables, years, save_dir, partition, num_shards_per_year
         os.path.join(save_dir, partition, "climatology.npz"),
         **climatology,
     )
+
 
 @click.command()
 @click.option("--root_dir", type=click.Path(exists=True))
@@ -169,7 +187,7 @@ def nc2np_daily(path, variables, years, save_dir, partition, num_shards_per_year
 @click.option("--start_test_year", type=int, default=2017)
 @click.option("--end_year", type=int, default=2019)
 @click.option("--num_shards", type=int, default=8)
-@click.option("--aggregation", type=str, default='mean')
+@click.option("--aggregation", type=str, default="mean")
 def main(
     root_dir,
     save_dir,
@@ -181,16 +199,26 @@ def main(
     num_shards,
     aggregation,
 ):
-    assert start_val_year > start_train_year and start_test_year > start_val_year and end_year > start_test_year
+    assert (
+        start_val_year > start_train_year
+        and start_test_year > start_val_year
+        and end_year > start_test_year
+    )
     train_years = range(start_train_year, start_val_year)
     val_years = range(start_val_year, start_test_year)
     test_years = range(start_test_year, end_year)
 
     os.makedirs(save_dir, exist_ok=True)
 
-    nc2np_daily(root_dir, variables, train_years, save_dir, "train", num_shards, aggregation)
-    nc2np_daily(root_dir, variables, val_years, save_dir, "val", num_shards, aggregation)
-    nc2np_daily(root_dir, variables, test_years, save_dir, "test", num_shards, aggregation)
+    nc2np_daily(
+        root_dir, variables, train_years, save_dir, "train", num_shards, aggregation
+    )
+    nc2np_daily(
+        root_dir, variables, val_years, save_dir, "val", num_shards, aggregation
+    )
+    nc2np_daily(
+        root_dir, variables, test_years, save_dir, "test", num_shards, aggregation
+    )
 
     # save lat and lon data
     ps = glob.glob(os.path.join(root_dir, variables[0], f"*{train_years[0]}*.nc"))
