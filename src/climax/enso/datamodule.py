afa9ec4e9c56a6d904c724d31f53e2be4cdad153
Alistair White <alistair@pik-potsdam.de>
2024-08-08 21:12:19 +0200

    Update dataset and datamodule

--- src/climax/enso/datamodule.py
+++ src/climax/enso/datamodule.py
@@ -1,26 +1,96 @@
+from datetime import date, timedelta
+
 from torch.utils.data import DataLoader
 from pytorch_lightning import LightningDataModule
+import xarray as xr
 
 from climax.enso.dataset import ENSODataset
 
 
 class ENSODataModule(LightningDataModule):
-    def __init__(self, data_dir: str, batch_size: int, lead_time: int):
+    def __init__(
+        self,
+        data_dir: str,
+        start_train_year: int,
+        start_val_year: int,
+        start_test_year: int,
+        end_year: int,
+        forecast_lead_time: int,
+        batch_size: int,
+        num_workers: int = 0,
+        pin_memory: bool = False,
+    ):
         super().__init__()
+
+        # Dataset args
         self.data_dir = data_dir
+        self.start_train_year = start_train_year
+        self.start_val_year = start_val_year
+        self.start_test_year = start_test_year
+        self.end_year = end_year
+        self.forecast_lead_time = forecast_lead_time
+
+        # Dataloader args
         self.batch_size = batch_size
-        self.lead_time = lead_time
+        self.num_workers = num_workers
+        self.pin_memory = pin_memory
+
+        # Set up datasets
+        # TODO: is here the best place to do this?
+        era5_dataset = xr.load_dataset(self.data_dir + "/era5_daily.nc")
+        enso_dataset = xr.load_dataset(self.data_dir + "/enso_daily.nc")
+        self.train_dataset = self.get_dataset(
+            era5_dataset,
+            enso_dataset,
+            self.start_train_year,
+            self.start_val_year - 1,
+        )
+        self.val_dataset = self.get_dataset(
+            era5_dataset,
+            enso_dataset,
+            self.start_val_year,
+            self.start_test_year - 1,
+        )
+        self.test_dataset = self.get_dataset(
+            era5_dataset,
+            enso_dataset,
+            self.start_test_year,
+            self.end_year,
+        )
 
     def setup(self, stage):
-        self.enso_train = ENSODataset("path/to/train", self.lead_time)
-        self.enso_val = ENSODataset("path/to/val", self.lead_time)
-        self.enso_test = ENSODataset("path/to/test", self.lead_time)
+        pass
+
+    def get_dataset(self, era5_dataset, enso_dataset, start_year, end_year):
+        start_date = date(start_year, 1, 1)
+        end_date = date(end_year, 12, 31)
+        leadtime = timedelta(days=self.forecast_lead_time)
+        era5 = era5_dataset.sel(time=slice(start_date, end_date)).to_array().to_numpy()
+        enso = enso_dataset.sel(time=slice(start_date - leadtime, end_date - leadtime)).to_array().squeeze().to_numpy()
+        lat = era5_dataset.lat.to_numpy()
+        lon = era5_dataset.lon.to_numpy()
+        return ENSODataset(era5, enso, lat, lon)
 
     def train_dataloader(self):
-        return DataLoader(self.enso_train, batch_size=self.batch_size)
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+        )
 
     def val_dataloader(self):
-        return DataLoader(self.enso_val, batch_size=self.batch_size)
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+        )
 
     def test_dataloader(self):
-        return DataLoader(self.enso_test, batch_size=self.batch_size)
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+        )
--- src/climax/enso/dataset.py
+++ src/climax/enso/dataset.py
@@ -1,22 +1,16 @@
-import xarray as xr
 import torch
 from torch.utils.data import Dataset
 
 
 class ENSODataset(Dataset):
-    def __init__(self, nc_file, lead_time):
-        era5_dataset = xr.load_dataset(nc_file)  # TODO: double check number types
-
-        self.era5_daily = torch.from_numpy(era5_dataset.to_array().to_numpy())  # V x T x W x H
-        self.lat = era5_dataset.lat.to_numpy()
-        self.lon = era5_dataset.lon.to_numpy()
-
-        self.enso_index = None  # TODO
-
-        self.lead_time = lead_time
+    def __init__(self, era5_daily, enso_index, lat, lon):
+        self.era5 = torch.from_numpy(era5_daily)  # V x T x W x H
+        self.enso = torch.from_numpy(enso_index)
+        self.lat = torch.from_numpy(lat)
+        self.lon = torch.from_numpy(lon)
 
     def __len__(self):
-        return self.era5_daily.shape[1] - self.lead_time
+        return len(self.enso)
 
     def __getitem__(self, idx):
-        return self.era5_daily[:, idx, :, :], self.enso_index[idx + self.lead_time]
+        return self.era5[:, idx, :, :], self.enso[idx]
