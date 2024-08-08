9efce1930c1f81e31e813b84a31a6e132160798e
Alistair White <alistair@pik-potsdam.de>
2024-08-08 21:15:51 +0200

    Add todo

--- src/climax/enso/datamodule.py
+++ src/climax/enso/datamodule.py
@@ -37,6 +37,7 @@ class ENSODataModule(LightningDataModule):
 
         # Set up datasets
         # TODO: is here the best place to do this?
+        # TODO: fix paths
         era5_dataset = xr.load_dataset(self.data_dir + "/era5_daily.nc")
         enso_dataset = xr.load_dataset(self.data_dir + "/enso_daily.nc")
         self.train_dataset = self.get_dataset(
@@ -64,7 +65,9 @@ class ENSODataModule(LightningDataModule):
     def get_dataset(self, era5_dataset, enso_dataset, start_year, end_year):
         start_date = date(start_year, 1, 1)
         end_date = date(end_year, 12, 31)
-        leadtime = timedelta(days=self.forecast_lead_time)
+        leadtime = timedelta(
+            days=self.forecast_lead_time
+        )  # TODO: throw error if start_date - leadtime is before the first date in the dataset
         era5 = era5_dataset.sel(time=slice(start_date, end_date)).to_array().to_numpy()
         enso = enso_dataset.sel(time=slice(start_date - leadtime, end_date - leadtime)).to_array().squeeze().to_numpy()
         lat = era5_dataset.lat.to_numpy()
